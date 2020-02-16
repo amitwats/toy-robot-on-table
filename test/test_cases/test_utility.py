a0f4221171b5a3b21ade67bf4df977455718f9f2
amitwats <amitwats@gmail.com>
2020-02-16 22:01:41 +1100

    added testes to utility

--- test/test_cases/test_utility.py
+++ test/test_cases/test_utility.py
@@ -5,5 +5,20 @@ import pytest
 import utility
 
 
+def test_validateType():
+    assert utility.validateType(5,int)==5
+    assert utility.validateType("5",str)=="5"
+    with pytest.raises(ValueError) as excinfo:
+        utility.validateType("5",int)
+
 def test_validateInteger():
-    
+    assert utility.validateType(5,int)==5
+    with pytest.raises(ValueError) as excinfo:
+        utility.validateType("A",int)
+
+def test_validateString():
+    assert utility.validateType("ABC",str)=="ABC"
+    with pytest.raises(ValueError) as excinfo:
+        utility.validateType(5,str)
+
+
